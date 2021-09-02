cf1c066359a19efb11f9ff3e9b165f7f57a25d40
codesbya <ajsingh73@gmail.com>
2021-09-02 17:56:49 -0500

    make it betterr

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -28,7 +28,7 @@ useEffect(() => {
     if(!currentTrack) return
 
     axios
-    .get("http://localhost:3001/lyrics", {
+    .get("https://musicapiapp.herokuapp.com/lyrics", {
         params: {
             track: currentTrack.title,
             artist: currentTrack.artist,
--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ import {Container} from 'react-bootstrap'
 // https://developer.spotify.com/documentation/web-api/
 // Creating the UTL from the docs in the link above
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=853556b1fbc44f33b98fab9d95d8eca9&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=853556b1fbc44f33b98fab9d95d8eca9&response_type=code&redirect_uri=https://musicapiapp.herokuapp.com&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 export default function Login (){
     return (
