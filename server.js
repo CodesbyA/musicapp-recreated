31996190db1634f68be1f362d53c94438b329423
codesbya <ajsingh73@gmail.com>
2021-09-02 15:17:48 -0500

    first

---
+++ server.js
@@ -0,0 +1,63 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require("cors");
+const WebApi = require('spotify-web-api-node');
+const bodyParser = require("body-parser")
+const lyricsFinder = require("lyrics-finder")
+
+const app = express();
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.json())
+
+  app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new WebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId:  process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken,
+    })
+
+spotifyApi
+.refreshAccessToken()
+.then(data => {
+res.json({
+    accessToken:data.body.accessToken,
+    expiresIn: data.body.expiresIn
+})      })
+    .catch(() => {
+        res.sendStatus(400)
+    })
+    })
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new WebApi({
+        redirectUri: process.env.REDIRECT_URI,
+        clientId:  process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+    })
+
+    spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in,
+        })
+    })
+    .catch(err => {
+        res.sendStatus(400)
+    })
+})
+
+app.get("/lyrics", async (req, res) => {
+    const lyrics = 
+    (await lyricsFinder(req.query.artist, req.query.track)) || "Lyrics Unavailable for this Song"
+    res.json({lyrics})
+})
+
+app.listen(3001)
---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Container} from 'react-bootstrap'
+
+// https://developer.spotify.com/documentation/web-api/
+// Creating the UTL from the docs in the link above
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=853556b1fbc44f33b98fab9d95d8eca9&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+export default function Login (){
+    return (
+    
+        // Center the button
+
+    <Container className="d-flex justify-content-center align-items-center" style={{minHeight: "100vh"}}>
+        <a className="btn btn-success btn-lg" href={AUTH_URL}>Login</a>
+        
+    </Container>
+    )
+}
